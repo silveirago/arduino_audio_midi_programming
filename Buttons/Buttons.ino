759a9572b4be297cfeb0dfe75c70e2a36d9c47a2
Gustavo Silveira <gustavo@MacBook-Pro-de-Gustavo.local>
2023-08-30 11:04:48 -0300

    buttons code added

--- Buttons/Buttons.ino
+++ Buttons/Buttons.ino
@@ -1,73 +1,68 @@
 /*
-This code reads the Arduino digital ports (buttons) and sends MIDI notes.
-by Gustavo Silveira, a.k.a. the Nerd Musician.
-Visit: https://go.musiconerd.com/nerd-musician-pro
+  This code reads the Arduino digital ports (buttons) and sends MIDI notes.
+  Created by Gustavo Silveira, a.k.a. the Nerd Musician.
+  Visit: https://go.musiconerd.com/nerd-musician-pro
 */
 
-#include "MIDIUSB.h"
+#include "MIDIUSB.h" // Include the MIDIUSB library for MIDI communication
 
+// Define the number of buttons
 const int N_BUTTONS = 3;
 
+// Define the Arduino pins for buttons and their corresponding MIDI note numbers
 int buttonPin[N_BUTTONS] = { 4, 3, 2 };
 int buttonNote[N_BUTTONS] = { 36, 38, 42 };
 
+// Arrays to store the current and previous button states
 int buttonState[N_BUTTONS] = { 0 };
 int buttonPState[N_BUTTONS] = { 0 };  // previous state of the button
 
+// Arrays to manage button debouncing
 unsigned long lastDebounceTime[N_BUTTONS] = { 0 };
 unsigned long debounceTimer[N_BUTTONS] = { 0 };
 int debounceDelay = 10;
 
-int BUTTON_CH = 0;
-
+int BUTTON_CH = 0; // MIDI channel for button events
 
 void setup() {
+  Serial.begin(9600); // Initialize serial communication for debugging
 
-  Serial.begin(9600);
-
+  // Configure each button pin as INPUT_PULLUP
   for (int i = 0; i < N_BUTTONS; i++) {
-
     pinMode(buttonPin[i], INPUT_PULLUP);
   }
 
-
-  // put your setup code here, to run once:
+  // Additional setup code can be added here
 }
 
 void loop() {
-
-  buttons();
+  buttons(); // Call the function to handle button events
 }
 
-// - - - - - - - - - - - - - - - - - - - - - - - -
-
+// Function to read and handle button states
 void buttons() {
-
   for (int i = 0; i < N_BUTTONS; i++) {
+    buttonState[i] = digitalRead(buttonPin[i]); // Read the current button state
 
-    buttonState[i] = digitalRead(buttonPin[i]);
+    debounceTimer[i] = millis() - lastDebounceTime[i]; // Calculate time since last state change
 
-    debounceTimer[i] = millis() - lastDebounceTime[i];
-
+    // Check if enough time has passed for debounce
     if (debounceTimer[i] > debounceDelay) {
+      if (buttonState[i] != buttonPState[i]) { // Check if button state has changed
+        lastDebounceTime[i] = millis(); // Update the debounce time
 
-      if (buttonState[i] != buttonPState[i]) {
-
-        lastDebounceTime[i] = millis();
-
+        // Check if the button is pressed
         if (buttonState[i] == LOW) {
-
-          noteOn(BUTTON_CH, buttonNote[i], 127);
-          MidiUSB.flush();
+          noteOn(BUTTON_CH, buttonNote[i], 127); // Send MIDI note-on message
+          MidiUSB.flush(); // Flush the MIDI data
 
           Serial.print("Button ");
           Serial.print(i);
           Serial.print(" ");
           Serial.println("on");
-        } else {
-
-          noteOn(BUTTON_CH, buttonNote[i], 0);
-          MidiUSB.flush();
+        } else { // Button is released
+          noteOn(BUTTON_CH, buttonNote[i], 0); // Send MIDI note-off message
+          MidiUSB.flush(); // Flush the MIDI data
 
           Serial.print("Button ");
           Serial.print(i);
@@ -75,19 +70,19 @@ void buttons() {
           Serial.println("off");
         }
 
-        buttonPState[i] = buttonState[i];
+        buttonPState[i] = buttonState[i]; // Update the previous button state
       }
     }
   }
 }
-// - - - - - - - - - - - - - - - - - - - - - - - -
 
-// Arduino (pro)micro midi functions MIDIUSB Library
+// Function to send a MIDI note-on message
 void noteOn(byte channel, byte pitch, byte velocity) {
   midiEventPacket_t noteOn = { 0x09, 0x90 | channel, pitch, velocity };
   MidiUSB.sendMIDI(noteOn);
 }
 
+// Function to send a MIDI control change message (not used in this code)
 void controlChange(byte channel, byte control, byte value) {
   midiEventPacket_t event = { 0x0B, 0xB0 | channel, control, value };
   MidiUSB.sendMIDI(event);
